590a9ed7e2c4f08f02f3104847853e6702a58f4c
paulinoacuna <paulinomaven911@gmail.com>
2020-12-22 17:23:46 -0500

    The Multitasking Man WEB APP CREATION

---
+++ text.js
@@ -0,0 +1,100 @@
+document.getElementById("n1").addEventListener("click",n1);
+document.getElementById("n2").addEventListener("click",n2);
+document.getElementById("n3").addEventListener("click",n3);
+document.getElementById("n4").addEventListener("click",n4);
+document.getElementById("n5").addEventListener("click",n5);
+document.getElementById("n6").addEventListener("click",n6);
+document.getElementById("n7").addEventListener("click",n7);
+document.getElementById("n8").addEventListener("click",n8);
+document.getElementById("n9").addEventListener("click",n9);
+document.getElementById("n0").addEventListener("click",n0);
+
+function n1(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n1').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n2(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n2').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n3(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n3').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n4(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n4').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n5(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n5').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n6(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n6').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n7(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n7').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n8(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n8').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n9(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n9').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
+
+function n0(){
+	var actual = document.getElementById('resultado').innerHTML;
+	var sumado = document.getElementById('n0').innerHTML;
+	
+	document.getElementById('resultado').innerHTML = actual + sumado;
+	
+
+}
